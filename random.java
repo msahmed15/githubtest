dd15af1949ee52eb1de7a785da082e56738dc914
msahmed15 <mansoor3238@gmail.com>
2023-10-28 12:51:31 -0500

    Create random.java

---
+++ random.java
@@ -0,0 +1,19 @@
+public class random
+{
+   public static void main(String [] args)
+   {
+      String x = "x";
+      String y = "x";
+      String z = new String("x");
+      String [] arr = { "12", "44", "w"};
+      Integer.parseInt( arr[2] );
+       
+      int t = 4;
+      int u = 12;
+      System.out.println( x == z );
+      
+      String j = "";
+      System.out.println ( 27/23  );
+      
+   }
+}
